4a6a369eb12f86232a4afb3a2d0557a733f9d702
Offroaders123 <65947371+Offroaders123@users.noreply.github.com>
2025-03-22 11:36:13 -0700

    Timestamp Abstraction

--- src/world_backup.rs
+++ src/world_backup.rs
@@ -1,4 +1,5 @@
-use std::{io::Result, time::SystemTime};
+use std::io::Result;
+use std::time::SystemTime;
 
 use chrono::{DateTime, Utc};
 use iso8601_timestamp::Timestamp;
@@ -6,14 +7,20 @@ use iso8601_timestamp::Timestamp;
 pub fn create_world_backup() -> Result<()> {
     println!("Making world backup...");
 
-    let timestamp: Timestamp = Timestamp::now_utc();
-    let system_time: SystemTime = timestamp.into();
-    let datetime: DateTime<Utc> = DateTime::from(system_time);
-    let formatted: String = datetime.format("%Y-%m-%d_%H.%M.%S").to_string();
+    let timestamp: String = create_backup_timestamp();
 
-    println!("{:?}", formatted);
+    println!("{timestamp}");
 
     println!("<to be implemented>");
 
     Ok(())
+}
+
+/// YYYY-MM-DD_HH.MM.SS
+fn create_backup_timestamp() -> String {
+    let timestamp: Timestamp = Timestamp::now_utc();
+    let system_time: SystemTime = timestamp.into();
+    let datetime: DateTime<Utc> = DateTime::from(system_time);
+    let formatted: String = datetime.format("%Y-%m-%d_%H.%M.%S").to_string();
+    formatted
 }
